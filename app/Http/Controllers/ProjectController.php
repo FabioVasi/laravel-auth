1b2a072b774e0645a6fb7fad9d03fef1ed4f039f
FabioVasi <vasifabio96@gmail.com>
2023-11-08 18:14:20 +0100

    Refactoring - Renamad Dashboardcontroller

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -13,7 +13,7 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        //
+        dd('cioa sono io il project controller index');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\Admin\ProjectController;
+use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ProjectController as ControllersProjectController;
 use Illuminate\Support\Facades\Route;
@@ -23,7 +23,7 @@ Route::get('/', function () {
 
 // admin routes // all routes start with: '/admin/..'
 Route::middleware(['auth', 'verified'])->prefix('admin')->name('admin.')->group(function () {
-    Route::get('/', [ProjectController::class, 'index'])->name('dashboard');
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
     
     //
     Route::resource('projects', ControllersProjectController::class);
