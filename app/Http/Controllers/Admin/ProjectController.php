1143d896514925296cc216056d434e310b7474a0
FabioVasi <vasifabio96@gmail.com>
2023-11-08 18:35:43 +0100

    Add table and seed

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -14,7 +14,7 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        dd('cioa sono io il project controller index');
+        dd('ciao sono io il project controller index');
     }
 
     /**
--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,16 +2,25 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 class ProjectSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        for ($i=0; $i < 10; $i++) { 
+            $project = new Project();
+            $project->title = $faker->realText(50);
+            $project->slug = Str::slug($project->title, '-');
+            $project->content = $faker->realText(50);
+            $project->save();
+        }
     }
 }
