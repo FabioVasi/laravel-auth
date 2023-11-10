03d4fe4ee3d5fbf40da003c758fb50e2043586d2
FabioVasi <vasifabio96@gmail.com>
2023-11-10 11:46:55 +0100

    add show card

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -35,57 +35,67 @@
                 <td>{{$project->id}}</td>
                 <td>
                     @if($project->image)
-                    <img src="{{asset('storage/' . $project->image)}}" alt="">
+                    <img height="180" src="{{asset($project->image)}}" alt="">
                     @else
                     N/A
                     @endif
                 </td>
-                <td><a href="{{$project->github}}"></a></td>
-                <td><a href="{{$project->second_link}}"></a></td>
+                <td><a href="{{$project->github}}">{{$project->github}}</a></td>
+                <td><a href="{{$project->second_link}}">{{$project->second_link}}</a></td>
                 <td>{{$project->title}}</td>
                 <td>
-                    <a href="{{route('admin.projects.show', $project)}}" class="btn btn-primary">View</a>
-                    <a href="{{route('admin.projects.edit', $project)}}" class="btn btn-secondary">Edit</a>
-                    
-                    <!-- Modal trigger button -->
-                    <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#deleteProject-{{$project->id}}">
-                      Delete
-                    </button>
-                    
-                    <!-- Modal Body -->
-                    <!-- if you want to close by clicking outside the modal, delete the last endpoint:data-bs-backdrop and data-bs-keyboard -->
-                    <div class="modal fade" id="deleteProject-{{$project->id}}" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false" role="dialog" aria-labelledby="modalTitleId-{{$project->id}}" aria-hidden="true">
-                        <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm" role="document">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="modalTitle-{{$project->id}}">{{$project->title}}</h5>
-                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body">
-                                    Delete Operation!
-                                </div>
-                                <div class="modal-footer">
+                    <ul class="list-unstyled m-0">
 
-                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <li>
+                            <a href="{{route('admin.projects.show', $project)}}" class="btn btn-primary">View</a>
+                        </li>
 
-                                    <form action="{{route('admin.projects.destroy', $project)}}" method="POST">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="btn btn-danger">Confirm</button>
-                                    </form>
-                                    
+                        <li class="my-4">
+                            <a href="{{route('admin.projects.edit', $project)}}" class="btn btn-secondary">Edit</a>
+                        </li>
+
+                        <li>
+                            <!-- Modal trigger button -->
+                            <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#deleteProject-{{$project->id}}">
+                            Delete
+                            </button>
+                            
+                            <!-- Modal Body -->
+                            <!-- if you want to close by clicking outside the modal, delete the last endpoint:data-bs-backdrop and data-bs-keyboard -->
+                            <div class="modal fade" id="deleteProject-{{$project->id}}" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false" role="dialog" aria-labelledby="modalTitleId-{{$project->id}}" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="modalTitle-{{$project->id}}">{{$project->title}}</h5>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            Delete Operation!
+                                        </div>
+                                        <div class="modal-footer">
+
+                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+
+                                            <form action="{{route('admin.projects.destroy', $project)}}" method="POST">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-danger">Confirm</button>
+                                            </form>
+                                            
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                    
-                    
-                    <!-- Optional: Place to the bottom of scripts -->
-                    <script>
-                        const myModal = new bootstrap.Modal(document.getElementById('modalId'), options)
-                    
-                    </script>
+                            
+                            
+                            <!-- Optional: Place to the bottom of scripts -->
+                            <script>
+                                const myModal = new bootstrap.Modal(document.getElementById('modalId'), options)
+                            
+                            </script>
+                        </li>
 
+                    </ul>
                 </td>
             </tr>
             @empty
--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -4,11 +4,26 @@
 
 @section('content')
 
-<h1>{{$project->title}}</h1>
 
-<img src="{{asset('storage/' . $project->image)}}" alt="">
-<div class="content">
-    <p>{{$project->content}}</p>
+
+<div class="container text-start">
+
+<h1 class="text-muted text-uppercase py-4">Project n°{{$project->id}}</h1>
+
+    <div class="row">
+        <div class="col-4">
+            <div class="card shadow">
+            <img  class="card-img-top" src="{{asset($project->image)}}" alt="">
+                <div class="card-body">
+                    <h4 class="py-3">{{$project->title}}</h4>
+                    <h6>{{$project->slug}}</h6>
+                    <p><a href="{{$project->github}}">{{$project->github}}</a></p>
+                    <p><a href="{{$project->second_link}}">{{$project->second_link}}</a></p>
+                    <p>{{$project->content}}</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 @endsection
